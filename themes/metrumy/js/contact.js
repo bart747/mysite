0ae99d70ace9f1a4e9f7ded44bb8dbafbbbc3d5f
Bartosz Wieczorek <hello.bartosz@gmail.com>
2017-10-27 21:36:28 +0200

    rewrite 'contact' JS for older browsers

--- themes/metrumy/js/contact.js
+++ themes/metrumy/js/contact.js
@@ -1,18 +1,21 @@
 
-const places = document.getElementsByClassName('liame');
+// Since it's about super crucial data I decided to use older JS.
+// It's an exception so everything else is setted up with modern JS in mind.
 
-const p1 = 'hello',
-      p2 = '.bartosz',
-      p3 = 'liamg';
+var places = document.getElementsByClassName('liame');
 
-const addr = p1 + p2 + '@' + rev(p3) + '.com'; 
+var p1 = 'hello',
+    p2 = '.bartosz',
+    p3 = 'liamg';
+
+var addr = p1 + p2 + '@' + rev(p3) + '.com'; 
 
 function rev(s) {
   return s.split('').reverse().join('');
 }
 
 (function create() {
-  let i = 0,
+  var i = 0,
       l = places.length;
   for (i = 0; i < l; i++) {
     places[i].innerHTML = "<a href='mailto:" + addr + "'>" + addr + '</a>';
