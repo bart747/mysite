da70a5d206fb5731a82f6309a32f1172371f2c47
Bartosz Wieczorek <hello.bartosz@gmail.com>
2017-11-17 00:50:15 +0100

    fix sw.js and links issues fetch after window load, fix link typos

--- themes/metrumy/js/main.js
+++ themes/metrumy/js/main.js
@@ -2,9 +2,11 @@ require('../sass/main.scss')
 require('./contact.js')
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(console.log('Service Worker Registered'))
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(console.log('Service Worker Registered'))
+  })
 }
 
 console.log("rock'n'roll!")
