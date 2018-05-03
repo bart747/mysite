66ce46a3abae2c7825f5c083e7c06268d64bd68f
Bartosz Wieczorek <hello.bartosz@gmail.com>
2018-05-03 22:49:23 +0200

    minor style editing

--- public/sw.js
+++ public/sw.js
@@ -1,52 +1,52 @@
 
-var cacheName = 'simple-cache'
-var cacheFiles = [
-  '/',
-  '/howiwork/',
-  '/offline/',
-  '/resilient-ui/',
-  '/research-decision/',
-  '/henry-ford-for-makers/',
-  '/manifest.json',
-  '/dist/bundle.js',
-  '/dist/bundle.css',
-  '/icon320.png'
-]
-var offlinePage = './offline/'
+const cacheName = "simple-cache";
+const cacheFiles = [
+  "/",
+  "/howiwork/",
+  "/offline/",
+  "/resilient-ui/",
+  "/research-decision/",
+  "/henry-ford-for-makers/",
+  "/manifest.json",
+  "/dist/bundle.js",
+  "/dist/bundle.css",
+  "/icon320.png",
+];
+const offlinePage = "./offline/";
 
-self.addEventListener('install', function (event) {
-  console.log('[ServiceWorker] Install')
+self.addEventListener("install", (event) => {
+  // console.log("[ServiceWorker] Install");
   event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log('[ServiceWorker] Caching preselected assets')
-      return cache.addAll(cacheFiles)
+    caches.open(cacheName).then((cache) => {
+      // console.log("[ServiceWorker] Caching preselected assets");
+      return cache.addAll(cacheFiles);
     })
-  )
-})
+  );
+});
 
-self.addEventListener('activate', function (event) {
-  console.log('[ServiceWorker] Activate')
+self.addEventListener("activate", (event) => {
+  // console.log("[ServiceWorker] Activate");
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
         if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key)
-          return caches.delete(key)
+          // console.log("[ServiceWorker] Removing old cache", key)
+          return caches.delete(key);
         }
-      }))
+      }));
     })
-  )
-  return self.clients.claim()
-})
+  );
+  return self.clients.claim();
+});
 
-self.addEventListener('fetch', function (event) {
-  console.log('[ServiceWorker] Fetch', event.request.url)
+self.addEventListener("fetch", (event) => {
+  // console.log("[ServiceWorker] Fetch", event.request.url);
   event.respondWith(
     // try network first, than cache, than offline page
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function (response) {
-        return response || caches.match(offlinePage)
-      })
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((response) => {
+        return response || caches.match(offlinePage);
+      });
     })
-  )
-})
+  );
+});
