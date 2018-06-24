1ff57e0219004c0182b01907658901c4b0662aa4
Bartosz Wieczorek <hello.bartosz@gmail.com>
2018-06-25 00:06:25 +0200

    change assets urls

---
+++ themes/metrumy/js/main.js
@@ -0,0 +1,11 @@
+require("../sass/main.scss");
+require("./contact.js");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      //.then(console.log("Service Worker Registered"))
+    ;
+  });
+}
